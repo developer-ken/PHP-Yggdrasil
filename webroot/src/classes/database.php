b9269d99573bf14188ec05c36da0f717b8b20228
dengbw01 <dengbw01@outlook.com>
2020-05-02 11:17:55 +0800

    纠正了一处数据库操作的错误

--- webroot/src/classes/database.php
+++ webroot/src/classes/database.php
@@ -178,7 +178,7 @@ class Database{
 
     function updateAllSessionState(){
         //清除创建超过30S的会话
-        return $this->query_rolls("delete from Sessions WHERE DATE(opentime) >= DATE_SUB(NOW(),INTERVAL 30 SECOND);",array());
+        return $this->query_rolls("delete from Sessions WHERE DATE(opentime) >= DATE_SUB(NOW(),INTERVAL ? SECOND);",array(30));
     }
 
     function closeSession($sid){
